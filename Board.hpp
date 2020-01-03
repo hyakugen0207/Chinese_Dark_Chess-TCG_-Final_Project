1fc93fe280be7972f48e611e56ae40e6f13f5cc7
hyakugen0207 <r07922135@ntu.edu.tw>
2020-01-03 18:14:19 +0800

    Version0.2.1 def Board function &     CNCNode mem

--- Board.hpp
+++ Board.hpp
@@ -1,5 +1,6 @@
 #include "myDefine.hpp"
 #include "Piece.hpp"
+#include <vector>
 
 class Board{
     public:
@@ -9,6 +10,12 @@ class Board{
         Piece* pieceList[2][16];
         int numPieces[2];
         int alivePieces[2][7];
+
+        std::vector<std::pair<char,char>> moveList();
+        bool move(char from, char dir);
+        void removePiece(int index, bool ply); //removePieceFromPieceList
+        int getScore();
+        bool isLegalMove(int from, int to, bool ply);
 };
 
 
@@ -18,4 +25,10 @@ class Board{
 - pieces : Piece[33]
 - pieceList : pointer of Piece [2][16]
 - numPieces[2] : int
+
++ moveList() : vector<pair<from,dir>>
++ move(from, dir) : bool
++ removePiece(int index, int color) : void // removePieceFromPieceList
++ getScore() : int
++ isLegalMove(int from, int to, bool turn) : bool
 */
--- CDCNode.hpp
+++ CDCNode.hpp
@@ -1,7 +1,13 @@
 #include "myDefine.hpp"
+#include "Board.hpp"
+#include <vec
 
 class CDCNode{
-    ULL hashValue;
-
+    public:
+        Board board;
+        ULL hashValue;
+        char move[6];
+        CDCNode* parent;
+        CDCNode* children[100]; //86? move 16*4 + jump 4 + flip 16
 };
 
